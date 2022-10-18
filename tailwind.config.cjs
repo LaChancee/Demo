66bf363975f0ca4682289ce80f28926339061e98
LaChancee <miharisoababef@gmail.com>
2022-10-18 22:26:54 +0200

    Page Recap et validation

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,11 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'ywl': '#067783',
+      }
+    }
   },
   plugins: [],
 }
